00748265b472b2c6fb8272cb9b3cee2e2397db92
DroppedHard <szyper7@gmail.com>
2023-01-07 19:53:39 +0100

    small equation changes

--- equations.py
+++ equations.py
@@ -4,7 +4,7 @@ import math
 equations = [
     ((lambda x,y,yprime : 5*yprime - 6*y, 0, 1, 1, math.e**2), "y\'\'=5y\'-6y, y(0)=1, y(1)=e^2", lambda x : math.e**(2*x)),
     ((lambda x,y,yprime : 2, 0, 10, 0, 0), "y\'\'=2, y(0)=0, y(10)=0", lambda x: x**2 - 10*x),
-    ((lambda x,y,yprime : -8*math.sin(x)*math.cos(x), 0, math.pi, 0, 0), "y\'\'=-8sin(x)cos(x), y(0)=0, y(π)=0", lambda x: math.sin(2*x)),
+    ((lambda x,y,yprime : -8*math.sin(x)*math.cos(x), 0, 2*math.pi, 0, 0), "y\'\'=-8sin(x)cos(x), y(0)=0, y(2π)=0", lambda x: math.sin(2*x)),
     ((lambda x,y,yprime : math.e**x - 6*x**2*math.sin(x) + x**3*math.cos(x) - 6*x*math.cos(x), 0, math.pi, 0, 0), "y\'\'=e^x-6x^2sin(x)+x^3cos(x)-6xcos(x), y(0)=0, y(π)=0", lambda x: math.e**x - math.cos(x)*x**3)
 ]
 
